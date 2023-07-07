54a5d8247164ff1802317ef8daf9e089bc628f40
mojy150 <mojy429@gmail.com>
2023-07-07 23:10:03 +0330

    change varible list_1 to questionToday_list

--- leitner.py
+++ leitner.py
@@ -11,14 +11,14 @@ engine.setProperty('rate', 170)
 basic_csv = 'basic.csv' # csv of word
 time_csv = 'time.csv' # csv of time
 
-def my_append(id_0,list_1,temp): # append random word in list_1
+def my_append(id_0,questionToday_list,temp): # append random word in questionToday_list
     while temp != 0:
         no_random = choice(id_0)
-        list_1.append(no_random)
+        questionToday_list.append(no_random)
         id_0.remove(no_random)
         temp -= 1
-    return list_1
-def check(file_csv,number,id_0,list_1): # check for new word or old word
+    return questionToday_list
+def check(file_csv,number,id_0,questionToday_list): # check for new word or old word
     id_0 = []
     with open(file_csv) as f:
         reader = csv.reader(f)    
@@ -40,9 +40,9 @@ def check(file_csv,number,id_0,list_1): # check for new word or old word
                 if row[3] == number:
                     id_0.append([row[0],row[1],row[2],row[3],'on'])
             temp = len(id_0)
-    list_1 = my_append(id_0,list_1,temp)
+    questionToday_list = my_append(id_0,questionToday_list,temp)
 
-def check_again(file_csv,number,list_1): # check again for new word or old word
+def check_again(file_csv,number,questionToday_list): # check again for new word or old word
     id_0 = []
     with open(file_csv) as f:
         reader = csv.reader(f)    
@@ -50,12 +50,12 @@ def check_again(file_csv,number,list_1): # check again for new word or old word
         if number == '0':
             for row in reader:
                 if row[3] == number and row[4] == 'on':
-                    list_1.append([row[0],row[1],row[2],row[3],row[4]]) 
+                    questionToday_list.append([row[0],row[1],row[2],row[3],row[4]]) 
         else:
             for row in reader:
                 if row[3] == number and row[4] == 'on':
-                    list_1.append([row[0],row[1],row[2],row[3],row[4]])
-    return list_1
+                    questionToday_list.append([row[0],row[1],row[2],row[3],row[4]])
+    return questionToday_list
 
 def last_id(): # give the last id
     n = int(0)
@@ -101,9 +101,9 @@ def edit_time_csv(filename,line0,line1,line2): # edit csv for time.csv
 
     shutil.move(tempfile.name, filename)    
 
-def leitner(list_1): # question words
+def leitner(questionToday_list): # question words
     sure = 'null'
-    for item in list_1:
+    for item in questionToday_list:
         if item[4] == 'on':
             print(item[1])
             engine.say(item[1])
@@ -126,7 +126,7 @@ def leitner(list_1): # question words
                 elif javab == 'y' or javab == 'Y' or javab == '':
                     item[3] , item[4] = str(int(item[3]) + 1) , 'off'
     return sure
-    # for row in list_1: # TODO
+    # for row in questionToday_list: # TODO
     #     edit_csv(basic_csv,row[0],row[1],row[2],row[3],row[4])
 
 def show():
@@ -142,7 +142,7 @@ def show():
 with open(basic_csv) as f:
     reader = csv.reader(f)
     id_0 = list() # list Ids in CSV
-    list_1 = list() # list word of question day
+    questionToday_list = list() # list word of question day
     list_another = list() # list word of does't question day
     while True:    
         init = input('do you want? (leitner[l]/input word in app[i]/show status[s]/off the app[o]): ')
@@ -169,7 +169,7 @@ with open(basic_csv) as f:
                     break
                 
         elif init == 'l' or init == 'L' or init == '': # leitner
-            list_1 = []
+            questionToday_list = []
             with open(time_csv) as time_tomorrow:
                 reader = csv.reader(time_tomorrow)
                 for row in reader:
@@ -177,15 +177,15 @@ with open(basic_csv) as f:
                 e = datetime.datetime.now()
                 if e.year > tomorrow_year or (e.year == tomorrow_year and e.month > tomorrow_month) or (e.year == tomorrow_year and e.month == tomorrow_month and e.day >= tomorrow_day):
                     print('you can use leitner now')
-                    check(basic_csv,'30',id_0,list_1)
-                    check(basic_csv,'15',id_0,list_1)
-                    check(basic_csv,'7',id_0,list_1)
-                    check(basic_csv,'3',id_0,list_1)
-                    check(basic_csv,'1',id_0,list_1)
-                    check(basic_csv,'0',id_0,list_1)
+                    check(basic_csv,'30',id_0,questionToday_list)
+                    check(basic_csv,'15',id_0,questionToday_list)
+                    check(basic_csv,'7',id_0,questionToday_list)
+                    check(basic_csv,'3',id_0,questionToday_list)
+                    check(basic_csv,'1',id_0,questionToday_list)
+                    check(basic_csv,'0',id_0,questionToday_list)
                     check(basic_csv,'another',id_0,list_another)
                     
-                    sure = leitner(list_1)
+                    sure = leitner(questionToday_list)
                     if sure != 'y' or sure != 'Y' or sure != '':
                         for row in list_another:
                             if (row[0] == '1' or row[0] == '3' or row[0] == '7' or row[0] == '15' or row[0] == '30') and row[4] == 'off':
@@ -197,34 +197,34 @@ with open(basic_csv) as f:
                     tomorrow = datetime.date.today() + datetime.timedelta(days=1) # TODO
                     edit_time_csv(time_csv,tomorrow.year,tomorrow.month,tomorrow.day)
                     
-                    if len(list_1) == 0:
+                    if len(questionToday_list) == 0:
                         print('len list is zero (0). ')
-                    # list_1.sort() # TODO
-                    # print(list_1)
+                    # questionToday_list.sort() # TODO
+                    # print(questionToday_list)
                     
-                    for row in list_1:
+                    for row in questionToday_list:
                         edit_csv(basic_csv,row[0],row[1],row[2],row[3],row[4])
                 else:
-                    list_1 = []
-                    check_again(basic_csv,'30',list_1)
-                    check_again(basic_csv,'15',list_1)
-                    check_again(basic_csv,'7',list_1)
-                    check_again(basic_csv,'3',list_1)
-                    check_again(basic_csv,'1',list_1)
-                    check_again(basic_csv,'0',list_1)
-                    if len(list_1) == 0:
+                    questionToday_list = []
+                    check_again(basic_csv,'30',questionToday_list)
+                    check_again(basic_csv,'15',questionToday_list)
+                    check_again(basic_csv,'7',questionToday_list)
+                    check_again(basic_csv,'3',questionToday_list)
+                    check_again(basic_csv,'1',questionToday_list)
+                    check_again(basic_csv,'0',questionToday_list)
+                    if len(questionToday_list) == 0:
                         print("you can'n use leitner now")
                         temp = input('but you can insert new word\n you want continue? (y/n): ')
                         if temp == 'y' or temp == 'Y' or temp == '':
-                            check(basic_csv,'0',id_0,list_1)
-                            sure = leitner(list_1) # tode (sure = or not)
-                            for row in list_1:
+                            check(basic_csv,'0',id_0,questionToday_list)
+                            sure = leitner(questionToday_list) # tode (sure = or not)
+                            for row in questionToday_list:
                                 edit_csv(basic_csv,row[0],row[1],row[2],row[3],row[4])
                     else:
                         print("you can just continue leitner now")
-                        sure = leitner(list_1) # tode (sure = or not)
-                        for row in list_1:
+                        sure = leitner(questionToday_list) # tode (sure = or not)
+                        for row in questionToday_list:
                             edit_csv(basic_csv,row[0],row[1],row[2],row[3],row[4])
-                        # print(list_1)
-                        if len(list_1) == 0:
+                        # print(questionToday_list)
+                        if len(questionToday_list) == 0:
                             print('len list is zero (0). ')
